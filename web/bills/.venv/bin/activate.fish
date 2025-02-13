39f177e2c2db388ba728c344beb730e27adabb77
Elkin Romero <elkinale@hotmail.com>
2025-02-13 13:03:57 -0500

    adding files

--- web/bills/.venv/bin/activate.fish
+++ web/bills/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/elkin/Documents/Python/utilities/bills/web/.venv
+set -gx VIRTUAL_ENV /home/elkin/Documents/Python/utilities/bills/web/bills/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
